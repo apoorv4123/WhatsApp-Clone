794cbc7678b0615b7f465a58253f36fff1366109
Apoorv Srivastava <apoorvsrivastava.4123@gmail.com>
2021-06-10 00:40:03 +0530

    chat list item

--- app/src/main/java/com/example/whatsappclone/ui/ChatActivity.kt
+++ app/src/main/java/com/example/whatsappclone/ui/ChatActivity.kt
@@ -1,5 +1,7 @@
 package com.example.whatsappclone.ui
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.whatsappclone.R
@@ -10,11 +12,29 @@ const val IMAGE = "image"
 
 
 class ChatActivity : AppCompatActivity() {
+
+    private val friendId by lazy {
+        intent.getStringExtra(UID)
+    }
+    private val name by lazy {
+        intent.getStringExtra(NAME)
+    }
+    private val image by lazy {
+        intent.getStringExtra(IMAGE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+    }
 
-
-
+    companion object {
+        fun createChatActivity(context: Context, id: String, name: String, image: String): Intent {
+            val intent = Intent(context, ChatActivity::class.java)
+            intent.putExtra(UID, id)
+            intent.putExtra(NAME, name)
+            intent.putExtra(IMAGE, image)
+            return intent
+        }
     }
 }
